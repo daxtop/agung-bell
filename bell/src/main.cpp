cde2977822c5764e3b6babaaae751a829e96f814
anung-git <anung93@ymail.com>
2021-03-31 20:15:43 +0700

    rtc

--- bell/src/main.cpp
+++ bell/src/main.cpp
@@ -1,6 +1,7 @@
 #include <Arduino.h>
 #include "DFPlayer.h"
 #include "Jadwal.h"
+#include "Rtc.h"
 #include <SoftwareSerial.h>
 
 // class init
@@ -16,9 +17,10 @@ Jadwal waktu07 = Jadwal(31);
 Jadwal waktu08 = Jadwal(36);
 Jadwal waktu09 = Jadwal(41);
 Jadwal waktu10 = Jadwal(46);
+Rtc time = Rtc();
 
 //variable
-unsigned char jam, menit;
+unsigned char jam, menit, once;
 
 // setup function
 void setup()
@@ -31,44 +33,56 @@ void setup()
 // main fuction
 void loop()
 {
-  if (waktu01.onTime(jam, menit))
+  //clock
+  unsigned char detik;
+  time.getTime(jam, menit, detik);
+  //cek jadwal
+  if (detik == 0 && once == 0)
   {
-    player.play(waktu01.getPlay());
+    once = 1;
+    if (waktu01.onTime(jam, menit))
+    {
+      player.play(waktu01.getPlay());
+    }
+    if (waktu02.onTime(jam, menit))
+    {
+      player.play(waktu02.getPlay());
+    }
+    if (waktu03.onTime(jam, menit))
+    {
+      player.play(waktu03.getPlay());
+    }
+    if (waktu04.onTime(jam, menit))
+    {
+      player.play(waktu04.getPlay());
+    }
+    if (waktu05.onTime(jam, menit))
+    {
+      player.play(waktu05.getPlay());
+    }
+    if (waktu06.onTime(jam, menit))
+    {
+      player.play(waktu06.getPlay());
+    }
+    if (waktu07.onTime(jam, menit))
+    {
+      player.play(waktu07.getPlay());
+    }
+    if (waktu08.onTime(jam, menit))
+    {
+      player.play(waktu08.getPlay());
+    }
+    if (waktu09.onTime(jam, menit))
+    {
+      player.play(waktu09.getPlay());
+    }
+    if (waktu10.onTime(jam, menit))
+    {
+      player.play(waktu10.getPlay());
+    }
   }
-  if (waktu02.onTime(jam, menit))
+  if (detik > 0)
   {
-    player.play(waktu02.getPlay());
-  }
-  if (waktu03.onTime(jam, menit))
-  {
-    player.play(waktu03.getPlay());
-  }
-  if (waktu04.onTime(jam, menit))
-  {
-    player.play(waktu04.getPlay());
-  }
-  if (waktu05.onTime(jam, menit))
-  {
-    player.play(waktu05.getPlay());
-  }
-  if (waktu06.onTime(jam, menit))
-  {
-    player.play(waktu06.getPlay());
-  }
-  if (waktu07.onTime(jam, menit))
-  {
-    player.play(waktu07.getPlay());
-  }
-  if (waktu08.onTime(jam, menit))
-  {
-    player.play(waktu08.getPlay());
-  }
-  if (waktu09.onTime(jam, menit))
-  {
-    player.play(waktu09.getPlay());
-  }
-  if (waktu10.onTime(jam, menit))
-  {
-    player.play(waktu10.getPlay());
+    once = 0;
   }
 }
